7539ff7023f83cde921bee30b3d126e6456ed7f6
Vidhi Patel <vidhilotus7@gmail.com>
2025-08-29 14:15:08 +0530

    Files Added

---
+++ script.js
@@ -0,0 +1,17 @@
+const project1 = document.getElementById("project1");
+
+const project2 = document.getElementById("project2");
+
+const project3 = document.getElementById("project3");
+
+project1.addEventListener("click", () => {
+  this.open("https://github.com/vidhisonani/netflix_clone/", "_blank");
+});
+
+project2.addEventListener("click", () => {
+  this.open("https://elearnplatformde.netlify.app/", "_blank");
+});
+
+project3.addEventListener("click", () => {
+  this.open("https://github.com/vidhisonani/calculator/", "_blank");
+});
