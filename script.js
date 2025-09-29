e8ca74fd1568eec14c934dbd4bada997be24000f
Vidhi Patel <vidhilotus7@gmail.com>
2025-09-29 15:04:35 +0530

    Project section updated

--- script.js
+++ script.js
@@ -1,19 +1,12 @@
 const project1 = document.getElementById("project1");
 const project2 = document.getElementById("project2");
-const project3 = document.getElementById("project3");
-const project4 = document.getElementById("project4");
-project1.addEventListener("click", () => {
-  this.open("https://github.com/vidhisonani/netflix_clone/", "_blank");
-});
 
-project2.addEventListener("click", () => {
+project1.addEventListener("click", () => {
   this.open("https://elearnplatformde.netlify.app/", "_blank");
 });
 
-project3.addEventListener("click", () => {
-  this.open("https://github.com/vidhisonani/calculator/", "_blank");
+project2.addEventListener("click", () => {
+  this.open("https://github.com/vidhisonani/web-mini-projects", "_blank");
 });
 
-project4.addEventListener("click", () => {
-  this.open("https://github.com/vidhisonani/digital_clock/", "_blank");
-});
+
