b60f92a9276f841590019ba47ecfba6ca62d5ef6
Vidhi Patel <vidhilotus7@gmail.com>
2026-01-01 12:08:48 +0530

    update

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 const project1 = document.getElementById("project1");
 const project2 = document.getElementById("project2");
 
-project1.addEventListener("click", () => {
-  this.open("https://elearnplatformde.netlify.app/", "_blank");
-});
+// project1.addEventListener("click", () => {
+//   this.open("https://elearnplatformde.netlify.app/", "_blank");
+// });
 
 project2.addEventListener("click", () => {
   this.open("https://github.com/vidhisonani/web-mini-projects", "_blank");
 });
 
+const year = new Date().getFullYear();
+const footer = document.getElementById("footer");
 
+if(footer){
+  footer.innerHTML += `<p>&copy; ${year} Vidhi Patel | Made with ❤️</p>`;
+}
