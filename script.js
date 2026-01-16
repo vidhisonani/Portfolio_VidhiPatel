cfca57ea15755163c1579a39c2f8a60c60a42a96
Vidhi Patel <vidhilotus7@gmail.com>
2026-01-16 16:11:40 +0530

    Add projects

--- script.js
+++ script.js
@@ -1,17 +1,12 @@
-const project1 = document.getElementById("project1");
-const project2 = document.getElementById("project2");
-
-// project1.addEventListener("click", () => {
-//   this.open("https://elearnplatformde.netlify.app/", "_blank");
-// });
-
-project2.addEventListener("click", () => {
-  this.open("https://github.com/vidhisonani/web-mini-projects", "_blank");
+document.querySelectorAll(".project").forEach(button => {
+  button.addEventListener("click", () => {
+    window.open(button.dataset.link, "_blank");
+  });
 });
 
 const year = new Date().getFullYear();
 const footer = document.getElementById("footer");
 
-if(footer){
+if (footer) {
   footer.innerHTML += `<p>&copy; ${year} Vidhi Patel | Made with ❤️</p>`;
 }
